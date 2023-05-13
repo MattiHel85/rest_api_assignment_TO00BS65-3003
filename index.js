30bceec1ac384e8ddc48eb644df568e0a36db2ff
Matt Simpson <70198997+MattiHel85@users.noreply.github.com>
2023-05-13 19:23:30 +0300

    add secret import to index.js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 require('dotenv').config() // Require this to hide MongoDB password
+const AWS = require('aws-sdk');
+const secretsManager = new AWS.SecretsManager();
+
+const secretName = 'mongo-password';
+const secret = await secretsManager.getSecretValue({ SecretId: secretName});
+const mySecret = JSON.parse(secret.SecretString);
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -6,42 +13,6 @@ const PORT = process.env.PORT;
 const mongoPass = process.env.PASSWORD; // MongoDB password imported from .env file
 const mongoose = require('mongoose')
 const routes = require('./routes.json')
-// Use this code snippet in your app.
-// If you need more information about configurations or implementing the sample code, visit the AWS docs:
-// https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/getting-started.html
-
-
-// import {
-//   SecretsManagerClient,
-//   GetSecretValueCommand,
-// } from "@aws-sdk/client-secrets-manager";
-
-// const secret_name = "mongo-password";
-
-// const client = new SecretsManagerClient({
-//   region: "eu-north-1",
-// });
-
-// let response;
-
-// try {
-//   response = await client.send(
-//     new GetSecretValueCommand({
-//       SecretId: secret_name,
-//       VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
-//     })
-//   );
-// } catch (error) {
-//   // For a list of exceptions thrown, see
-//   // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-//   throw error;
-// }
-
-// const secret = response.SecretString;
-
-// console.log(secret)
-
-// Your code goes here
 
 // DB Connection address
 const uri = `mongodb+srv://mattisimpson:${mongoPass}@to00bs65-3003.gj3nvll.mongodb.net/?retryWrites=true&w=majority`;
@@ -56,6 +27,8 @@ const db = mongoose.connection;
 db.on("error", (err) => console.log(`Connection ${err}`));
 
 db.once("open", () => console.log("Connected to DB!"));
+
+console.log(mySecret);
 
 // Don't delete this again. You nee
 app.use(express.json());
