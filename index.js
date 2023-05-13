57baa0acb2534fbc1e369488374524c3ef8ff163
Matt Simpson <70198997+MattiHel85@users.noreply.github.com>
2023-05-13 15:03:34 +0300

    AWS secret test

--- index.js
+++ index.js
@@ -2,10 +2,48 @@ require('dotenv').config() // Require this to hide MongoDB password
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const PORT = process.env.PORT;
-const mongoPass = process.env.PASSWORD; // MongoDB password imported from .env file
+const PORT = process.env.PORT || 3000;
+const mongoPass = process.env.PASSWORD || "_72vLRW_fv9n!ty"; // MongoDB password imported from .env file
 const mongoose = require('mongoose')
 const routes = require('./routes.json')
+// Use this code snippet in your app.
+// If you need more information about configurations or implementing the sample code, visit the AWS docs:
+// https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/getting-started.html
+
+// PORT=3000
+// PASSWORD=_72vLRW_fv9n!ty
+
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
+
+const secret_name = "mongo-password";
+
+const client = new SecretsManagerClient({
+  region: "eu-north-1",
+});
+
+let response;
+
+try {
+  response = await client.send(
+    new GetSecretValueCommand({
+      SecretId: secret_name,
+      VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+    })
+  );
+} catch (error) {
+  // For a list of exceptions thrown, see
+  // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+  throw error;
+}
+
+const secret = response.SecretString;
+
+console.log(secret)
+
+// Your code goes here
 
 // DB Connection address
 const uri = `mongodb+srv://mattisimpson:${mongoPass}@to00bs65-3003.gj3nvll.mongodb.net/?retryWrites=true&w=majority`;
